c897b0e38386f48f94441dbcf55fc401d532aa70
Evan Tung <ejtung@amazon.com>
2025-08-18 22:23:03 -0400

    Revert "feat: add youtube event fetching with a basic script to fetch a predetermined committee event"
    This reverts commit 7cc553add0f91ddfe838ce257277e331154d591e.

--- projects/1.2-committee-youtube/congress_youtube/youtube/fetch/main.py
+++ projects/1.2-committee-youtube/congress_youtube/youtube/fetch/main.py
@@ -1,27 +1,10 @@
-import polars as pl
-from pathlib import Path
 from ...auth import load_youtube_api_key
 from .youtube_event_fetcher import YoutubeEventFetcher
 
 
 def main():
-    committee, title = "Appropriations", "Budget Hearing – U.S. Department of Health and Human Services"
-
+    fetcher = YoutubeEventFetcher()
     api_key = load_youtube_api_key()
-    fetcher = YoutubeEventFetcher(api_key)
-
-    youtube_accounts = pl.read_csv(Path(__file__).parent.parent / "youtube-accounts.csv")
-
-    appropriations_handle = youtube_accounts.filter(pl.col("Committee Name") == committee).row(0, named=True)["handle"]
-
-    appropriations_channel = fetcher.get_channel(appropriations_handle)
-
-    video = fetcher.get_event(
-        title=title,
-        channel_id=appropriations_channel["id"]
-    )
-    print(video)
-    print(f"Recording URL: https://youtu.be/{video['id']['videoId']}")
 
 
 if __name__ == "__main__":
