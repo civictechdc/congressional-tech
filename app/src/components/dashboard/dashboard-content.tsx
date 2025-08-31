4e1f28644d1ab8c09b734b786523f32b66c2973d
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-08-31 15:21:00 -0400

    clean up annotation and labeling

--- app/src/components/dashboard/dashboard-content.tsx
+++ app/src/components/dashboard/dashboard-content.tsx
@@ -1,18 +1,46 @@
 "use client";
 import useYoutubeEventIdReport from "@/hooks/use-youtube-event-id-report";
 import { StackedBarChart } from "./stacked-bar-chart";
+import congressMetadata from "@/data/congress_metadata.json";
+import { LucideArrowBigRight, LucideArrowRight } from "lucide-react";
 
 export function DashboardContent({}) {
     const { data, error, isError, isLoading } = useYoutubeEventIdReport();
 
     if (isError || isLoading || !data) return null;
 
-    const data119 = data.filter((eventIdRow) => eventIdRow.congress_number === 119);
-    const data118 = data.filter((eventIdRow) => eventIdRow.congress_number === 118);
+    const congressNumbers = Array.from(
+        new Set(
+            data
+                .filter((eventIdRow) => eventIdRow.total_videos > 0)
+                .map((eventIdRow) => eventIdRow.congress_number)
+        )
+    )
+        .sort((a, b) => a - b)
+        .reverse();
     return (
         <div className="grid w-screen grid-cols-2 gap-4 p-4 md:grid-cols-4">
-            <StackedBarChart className="col-span-2" data={data119} />
-            <StackedBarChart className="col-span-2" data={data118} />
+            {congressNumbers.map((congressNumber) => {
+                const startString = `${congressMetadata[congressNumber.toString()].start}`;
+                const endString = `${congressMetadata[congressNumber.toString()].end}`;
+                const subtitle = (
+                    <p className="flex flex-row items-center gap-1">
+                        {startString} <LucideArrowRight size={16} /> {endString}
+                    </p>
+                );
+                return (
+                    <StackedBarChart
+                        chartMeta={{
+                            title: `${congressNumber}th Congress`,
+                            subtitle,
+                        }}
+                        className="col-span-2"
+                        data={data.filter(
+                            (eventIdRow) => eventIdRow.congress_number === congressNumber
+                        )}
+                    />
+                );
+            })}
         </div>
     );
 }
