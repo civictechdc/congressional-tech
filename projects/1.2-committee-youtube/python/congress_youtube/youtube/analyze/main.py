0ef29e7a6f9e110b937ea23d60d7da27a87b0fc6
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-08-30 20:32:37 -0400

    hookup default overrides by centralizing arg logic for fetcher and analyzer

--- projects/1.2-committee-youtube/python/congress_youtube/youtube/analyze/main.py
+++ projects/1.2-committee-youtube/python/congress_youtube/youtube/analyze/main.py
@@ -1,25 +1,26 @@
 from pathlib import Path
 import re
 import csv
-from tinydb import TinyDB, Query
-from tinydb.table import Table
+from tinydb import Query
 import logging
 from dataclasses import asdict, dataclass
+from ...globals import add_global_args, add_youtube_args
 
 import argparse
+
 from ..tables import (
     get_all_commitee_names,
+    get_all_committee_handless,
     open_tinydb_for_committee,
-    get_committee_handles,
 )
-
-DEFAULT_OUTPUT_FILE = (
-    Path(__file__).resolve().parent.parent.parent.parent.parent
-    / "data"
-    / "event_id_report.csv"
+from ...globals import (
+    DEFAULT_YOUTUBE_REPORT_FILE,
+    DEFAULT_TINYDB_DIR,
+    DEFAULT_CHANNELS_CSV,
 )
 
 EVENT_ID_REGEX = ".*(\\d{6}|eventid).*"
+
 
 ## define columns in row of final report
 @dataclass
@@ -30,13 +31,27 @@ class EventIdReport:
     missing_event_id: int
 
 
-def main(output_path:Path=DEFAULT_OUTPUT_FILE) -> None:
+def main(
+    output_path: Path = DEFAULT_YOUTUBE_REPORT_FILE,
+    tinydb_dir: Path = DEFAULT_TINYDB_DIR,
+    channels_csv_path: Path = DEFAULT_CHANNELS_CSV,
+) -> None:
     report: list[EventIdReport] = []
-    committee_names = get_all_commitee_names(with_index=True)
-    for committee in committee_names:
+    ## load all the names & their indices
+    committee_names = get_all_commitee_names(
+        with_index=True, csv_path=channels_csv_path
+    )
+    ## load all the corresponding handles
+    committee_handless = get_all_committee_handless(channels_csv_path)
+    for committee_name in committee_names:
         try:
-            db = open_tinydb_for_committee(committee[1])
-            handles = get_committee_handles(committee[0])
+            db = open_tinydb_for_committee(
+                committee_name_or_index=committee_name[1],
+                csv_path=channels_csv_path,
+                tinydb_dir=tinydb_dir,
+                assert_exists=True,
+            )
+            handles = committee_handless[committee_name[1]]
             ## skip when we're in a row that has fewer handles than the max # (-> empty column)
             for handle in handles:
                 if handle == "":
@@ -55,10 +70,11 @@ def main(output_path:Path=DEFAULT_OUTPUT_FILE) -> None:
                 ## add the row
                 report.append(
                     EventIdReport(
-                        committee[0], ## committee name, repeats for multiple handles
-                        handle, ## this handle
-                        total_videos, ## all videos
-                        total_videos - has_event_id_count, ## bad videos
+                        ## committee name, repeats for multiple handles
+                        committee_name[0],
+                        handle,  ## this handle
+                        total_videos,  ## all videos
+                        total_videos - has_event_id_count,  ## bad videos
                     )
                 )
         except ValueError as e:
@@ -71,17 +87,30 @@ def write_to_csv(report: list[EventIdReport], output_path: Path):
     if len(report) == 0:
         return
     field_names = list(report[0].__annotations__.keys())
-    with open(output_path, mode="w", newline='') as file:
+    with open(output_path, mode="w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=field_names)
 
         writer.writeheader()
         for row in report:
             writer.writerow(asdict(row))
-            
+
 
 def parse_args_and_run():
     parser = argparse.ArgumentParser(
         description="Generates report on committee videos with missing event ids"
+    )
+
+    ## add shared args to the parser
+    add_global_args(parser)
+
+    ## add youtube specific args
+    add_youtube_args(parser)
+
+    parser.add_argument(
+        "--output-path",  ## dashes are automatically converted to underscores
+        type=Path,
+        default=DEFAULT_YOUTUBE_REPORT_FILE,
+        help="Path to the output CSV file.",
     )
 
     ## ignore the unknown args
@@ -92,4 +121,3 @@ def parse_args_and_run():
 
 if __name__ == "__main__":
     parse_args_and_run()
-
