ea38d362a8553ff4e5165ea198a0a0c9e0923ac7
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-08-31 14:06:26 -0400

    add stacked bar chart as cards in dashboard grid layout

--- app/src/components/dashboard/dashboard-content.tsx
+++ app/src/components/dashboard/dashboard-content.tsx
@@ -1,8 +1,17 @@
 "use client";
 import useYoutubeEventIdReport from "@/hooks/use-youtube-event-id-report";
+import { StackedBarChart } from "./stacked-bar-chart";
 
 export function DashboardContent({}) {
     const { data, error, isError, isLoading } = useYoutubeEventIdReport();
-    console.log(data, isLoading);
-    return <div></div>;
+
+    if (isError || isLoading || !data) return null;
+
+    const subsetData = data.filter((eventIdRow) => eventIdRow.congress_number === 119);
+    return (
+        <div className="grid grid-cols-4 gap-4 p-4">
+            <StackedBarChart className="col-span-2" data={subsetData} />
+            <StackedBarChart className="col-span-2" data={subsetData} />
+        </div>
+    );
 }
