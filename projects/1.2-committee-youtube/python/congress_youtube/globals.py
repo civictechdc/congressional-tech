8dd214c3004d168a8e2e3d5e2886265ae2a2bec4
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-08-30 20:18:26 -0400

    simplify identifying committees & accessing their tinydbs by centralizing the logic

--- projects/1.2-committee-youtube/python/congress_youtube/globals.py
+++ projects/1.2-committee-youtube/python/congress_youtube/globals.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 from argparse import ArgumentParser
 
-DEFAULT_TINYDB_PATH = (
-    Path(__file__).resolve().parent.parent.parent / "data" / "congress_youtube_db.json"
-)
+DEFAULT_TINYDB_DIR = Path(__file__).resolve().parent.parent.parent / "data"
 DEFAULT_CHANNELS_CSV = (
     Path(__file__).resolve().parent / "youtube" / "youtube-accounts.csv"
 )
@@ -13,8 +11,8 @@ def add_global_args(parser: ArgumentParser) -> None:
     """Add shared arguments to an argparser for re-use throughout different modules."""
 
     parser.add_argument(
-        "--tinydb_path",
+        "--tinydb_dir",
         type=lambda x: Path(x).expanduser().resolve(),
-        default=DEFAULT_TINYDB_PATH,
-        help="Path to the TinyDB database json file.",
+        default=DEFAULT_TINYDB_DIR,
+        help="Path to the directory containing TinyDB database json files.",
     )
