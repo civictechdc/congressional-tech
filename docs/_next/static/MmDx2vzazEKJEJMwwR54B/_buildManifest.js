3872cb6fe731b4f521b062707da8cce17ebc996f
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-09-01 02:37:22 -0400

    update basePath for dashboard data fetch

---
+++ docs/_next/static/MmDx2vzazEKJEJMwwR54B/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/48e9362babaaa731.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/congressional-tech//_next/:path+",
+        "destination": "/congressional-tech/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/dist/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/node_modules_0e60d9f9._.js",
-  "static/chunks/src_79604fe6._.js"
-],
-    source: "dynamic"
-});
