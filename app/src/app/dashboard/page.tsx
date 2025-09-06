d32221f70016c83af5db0d226db7016ada9713d5
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-09-06 18:25:22 -0400

    if there are 100 people who hate flex boxes i am one of them. if there is one person who hates flex boxes, that will be me.

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -2,10 +2,7 @@ import { DashboardContent } from "@/components/dashboard/dashboard-content";
 
 export default function DashboardPage() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
-            <h1 className="text-3xl font-bold text-gray-900">
-                House Congressional YouTube EventID Linkage Tracker
-            </h1>
+        <main className="flex min-h-screen flex-col bg-gray-50">
             <DashboardContent />
         </main>
     );
