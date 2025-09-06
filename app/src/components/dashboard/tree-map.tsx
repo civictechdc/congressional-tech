f82b5437bf3eada0fcdcb62bbc2edb099f5162d5
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-09-06 16:44:03 -0400

    stop tooltip from flickering! hooray!

--- app/src/components/dashboard/tree-map.tsx
+++ app/src/components/dashboard/tree-map.tsx
@@ -6,11 +6,20 @@ import colors from "tailwindcss/colors";
 const GREEN = colors.green[600];
 const RED = colors.red[600];
 
+type TooltipContent = {
+    committeeName: string;
+    handle: string;
+    total_videos: number;
+    missing_event_id: number;
+    missingFraction: string;
+};
+
 type TreeNode = {
-    name: string;
+    name?: string;
     children?: TreeNode[];
     size?: number;
     fill?: string;
+    tooltip?: TooltipContent;
 };
 
 // Recharts Treemap `content` expects a ReactElement, so we provide a component
@@ -44,16 +53,15 @@ function TreemapNodeWithGap({
 
     return (
         <g>
-            {depth > 0 && (
+            {depth > 1 && (
                 <>
+                    <rect x={x} y={y} width={width} height={height} fill="rgba(0,0,0,0)" />
                     <rect
                         x={x0}
                         y={y0}
                         width={depth > 1 ? w - 2 * (depth == 1 ? 0 : 2) : w}
                         height={depth > 1 ? h - 2 * (depth == 1 ? 0 : 2) : h}
-                        fill={fill ?? RED}
-                        stroke={depth == 1 ? "black" : "transparent"}
-                        strokeWidth={depth == 1 ? 0 : 0}
+                        fill={fill ?? "none"}
                     />
                     {w > 40 && h > 12 && rest?.name && (
                         <g>
@@ -106,36 +114,30 @@ export function TreeMap({
     }, {});
 
     const treemapData: TreeNode[] = Object.entries(committeesMap).map(([committeeName, rows]) => ({
-        name: "foo bar",
-        fill: "blue",
-        size: 5000,
         children: rows.map((row) => {
             const missingFraction = row.missing_event_id / row.total_videos;
             const tooltipContent = {
                 committeeName: row.committee_name,
                 handle: row.handle,
-                totalVideos: row.total_videos,
+                total_videos: row.total_videos,
+                missing_event_id: row.missing_event_id,
                 missingFraction: (100 * missingFraction).toFixed(2),
-            };
+            } as TooltipContent;
             return {
-                name: row.handle,
-                tooltip: tooltipContent,
-                size: row.total_videos,
+                name: row.handle, // text that is rendered inside the node
+                fill: RED,
                 children: [
                     {
-                        name: null,
                         tooltip: tooltipContent,
                         size: row.total_videos * missingFraction,
-                        fill: "transparent",
                     },
                     {
-                        name: null,
                         tooltip: tooltipContent,
                         size: row.total_videos * (1 - missingFraction),
                         fill: GREEN,
                     },
                 ],
-            };
+            } as TreeNode;
         }),
     }));
 
@@ -157,13 +159,21 @@ export function TreeMap({
                             trigger={"click"}
                             filterNull={false}
                             content={({ active, payload, ...rest }) => {
-                                console.log(active, payload?.[0], rest);
+                                const tooltip = payload?.[0]?.payload?.tooltip;
                                 return (
-                                    <div className="rounded border border-gray-200 bg-white p-3 shadow-md">
-                                        <div className="font-semibold">{`Committee: ${payload?.[0]?.payload?.tooltip?.committeeName}`}</div>
-                                        <div>{`Handle: ${payload?.[0]?.payload?.tooltip?.handle}`}</div>
-                                        <div>{`Total Videos: ${payload?.[0]?.payload?.tooltip?.totalVideos}`}</div>
-                                        <div>{`Missing Fraction: ${payload?.[0]?.payload?.tooltip?.missingFraction}%`}</div>
+                                    <div className="grid gap-1 rounded border border-gray-200 bg-white p-3 shadow-md">
+                                        <div className="text-center font-semibold">{`${payload?.[0]?.payload?.tooltip?.committeeName}`}</div>
+                                        <div className="text-center">{`${tooltip?.handle}`}</div>
+                                        <div className="grid grid-cols-2 gap-4">
+                                            <div className="text-center">
+                                                <div className="font-medium">Missing</div>
+                                                <div>{`${tooltip?.missing_event_id}/${tooltip?.total_videos}`}</div>
+                                            </div>
+                                            <div className="text-center">
+                                                <div className="font-medium">Percentage</div>
+                                                <div>{`${tooltip?.missingFraction}%`}</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 );
                             }}
