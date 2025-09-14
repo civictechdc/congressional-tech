805081b7fa45674ee268bd4239e306cafcd245b3
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-09-14 15:37:09 -0400

    feat: add classes for loading and parsing the committee data

---
+++ projects/1.2-committee-youtube/python/congress_youtube/congress/analyze/main.py
@@ -0,0 +1,55 @@
+import argparse
+
+from ...globals import add_global_args
+from ..fetch.congress_event_fetcher import CongressEventFetcher
+
+
+def main(tinydb_dir: str, chamber: str = "house", congress_number: int = 119):
+    ## pass a dummy api_key so we can access tinydb tables
+    fetcher = CongressEventFetcher("", tinydb_dir)
+    committees = fetcher.committees_tb.all()
+    for committee in committees:
+        parent = committee.get("parent", None)
+        if parent is None:
+            msg = f"Self is: {committee.get('systemCode')}"
+        else:
+            msg = f"Parent is: {parent['systemCode']}"
+        print(msg)
+
+    # fetcher.fetch_event_list(congress_number, chamber)
+    # fetcher.process_events()
+
+
+def parse_args_and_run():
+    parser = argparse.ArgumentParser(
+        description="Fetch all events from a chamber of the US Congress and store them in a TinyDB database."
+    )
+
+    ## add shared args to the parser
+    add_global_args(parser)
+
+    parser.add_argument(
+        "--chamber",
+        type=str,
+        choices=["house", "senate", "nochamber"],
+        default="house",
+        help="The chamber of Congress whose calendar to pull from.",
+    )
+
+    parser.add_argument(
+        "--congress_number",
+        type=int,
+        choices=range(100, 120),
+        default=119,
+        help="The session of Congress to pull data from (NOTE: only tried 119, not sure how early you can go back).",
+    )
+
+    ## ignore the unknown args
+    args = parser.parse_known_args()[0]
+
+    print(args)
+    main(**vars(args))
+
+
+if __name__ == "__main__":
+    parse_args_and_run()
