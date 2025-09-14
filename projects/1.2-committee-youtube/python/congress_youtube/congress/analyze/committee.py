11d3c2e2cb0f94d535b5bcd09c482126ed399268
Alex Gurvich <alex.b.gurvich@gmail.com>
2025-09-14 18:07:00 -0400

    feat: add details fetching and caching w/ central Committee class

---
+++ projects/1.2-committee-youtube/python/congress_youtube/congress/analyze/committee.py
@@ -0,0 +1,29 @@
+from .committee_details import CommitteeDetails
+from .committee_summary import CommitteeSummary
+
+
+class Committee:
+    summary: CommitteeSummary = None
+    details: CommitteeDetails = None
+
+    def __init__(self, chamber):
+        self.chamber = chamber
+
+    @classmethod
+    def from_summary(cls, summary: CommitteeSummary, chamber: str):
+        ## TODO: replace chamber with validation and Literal; should extract that...
+        inst = cls(chamber)
+        ## bind the summary
+        inst.summary = summary
+        return inst
+
+    def get_details(self, api_key: str, force_fetch: bool = False):
+        if self.details is not None:
+            return self.details
+
+        ## will load from the tinydb before fetching
+        self.details = CommitteeDetails.from_spec(
+            self.chamber, self.summary.systemCode, api_key, force_fetch
+        )
+
+        return self.details
