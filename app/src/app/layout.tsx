c2391912f6cd3e9c7de141e5ce211b295c2d78bc
fwliu1 <46015578+fwliu1@users.noreply.github.com>
2025-09-17 11:16:27 -0400

    Update layout.tsx
    Deleted the comment versio

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -25,36 +25,7 @@ export const metadata: Metadata = {
     title: "Congressional YouTube",
     description: "Simple static app exploring congressional committee YouTube content.",
 };
-/*
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-            <body
-                className={`flex flex-col ${geistSans.variable} ${geistMono.variable} overflow-hidden antialiased`}
-            >
-                <ReactQueryProvider>
-                    <div className="z-10 flex flex-1 flex-row items-center p-2">
-                        <Link href="/">
-                            <Image
-                                src={favicon}
-                                alt="Favicon"
-                                className="h-8 w-auto cursor-pointer"
-                            />
-                        </Link>
-                        <NavBar />
-                    </div>
-                    <div className="overflow-y-scroll">{children}</div>
-                </ReactQueryProvider>
-            </body>
-        </html>
-    );
-}
 
-*/
 export default function RootLayout({
     children,
 }: Readonly<{
