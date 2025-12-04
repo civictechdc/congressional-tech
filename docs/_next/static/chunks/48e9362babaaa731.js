0b05d5de09caee5b600394d43669ad583ce29f47
Evan Tung <ejtung@amazon.com>
2025-12-03 20:26:52 -0500

    fix: use turborepo for monorepo management

--- docs/_next/static/chunks/48e9362babaaa731.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/8f4f20723b842827.js",
-  "static/chunks/turbopack-1e17dd0954ffa762.js"
-])
--- projects/1.2-committee-youtube/fetch_all_youtube_videos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {0..12}
-do
-    youtube-fetch --tinydb_path data/youtube${i}.json -i $i
-done
