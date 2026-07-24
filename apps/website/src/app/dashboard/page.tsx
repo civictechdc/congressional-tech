53ea06f8e9e85204646f2a1ff9c0092858df3433
mikewolfd <worldestroyer@gmail.com>
2026-07-24 17:31:52 -0400

    chore: retire apps/website (Next app superseded by the apps/site Astro cutover)

--- apps/website/src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardContent } from "@/components/dashboard/dashboard-content";
-
-export default function DashboardPage() {
-    return (
-        <main className="flex min-h-screen flex-col bg-gray-50">
-            <DashboardContent />
-        </main>
-    );
-}
